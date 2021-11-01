c7bebfeb39fa4c2fcd00b0418ed8d8297a4649f6
roland <rj1337@gmail.com>
2021-11-01 13:52:22 +0900

    preload profile pic, aria-label on links

--- src/App.js
+++ src/App.js
@@ -30,11 +30,11 @@ function App() {
 
 <VisibilityComponent children={<Fragment> <h1  style={{
     padding:'100px'
-  }} >{t('Hello👋, my name is Roland Matheson')}  <br/> <Socials/> </h1> <Image alt='profile picture of Roland' style={{width:'200px'}} src={profile} roundedCircle/> </Fragment>} />
+  }} >{t('Hello👋, my name is Roland Matheson')}  <br/> <Socials/> </h1> <Image style={profile ? null : { display: 'none' }} alt='profile picture of Roland' style={{width:'200px'}} src={profile} roundedCircle/> </Fragment>} />
 
-<VisibilityComponent children={ <h3  style={{
+<VisibilityComponent children={ <h2  style={{
     padding:'100px',
-  }} ><b>{t('a Junior Fullstack Web Developer')} </b> </h3> } />
+  }} ><b>{t('a Junior Fullstack Web Developer')} </b> </h2> } />
 
 
 <VisibilityComponent children={<Text text={`I am a self taught Fullstack Web Developer since April 2021 currently learning the MERN tech stack. In January of 2021 I graduated with a Bachelors in Political Science but decided to learn software development after wanting to pursue a career as a software engineer. 
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -4,10 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function Socials() {
     return (
         <Fragment>
-            <a alt='link to Rolands Github' href='https://github.com/rj13371'><FontAwesomeIcon color='black' style={{marginRight:'15px'}} icon={['fab', 'github']} size="lg" /></a>
-            <a alt='link to Rolands linkedin' href='https://www.linkedin.com/in/roland-matheson-b37a09224/'><FontAwesomeIcon color='black' style={{marginRight:'15px'}} icon={['fab', 'linkedin']} size="lg" /></a>
-            <a alt='link to Rolands medium' href='https://rolandmatheson.medium.com/'><FontAwesomeIcon color='black' style={{marginRight:'15px'}} icon={['fab', 'medium']} size="lg" /></a>
-            <a alt='email for rolands gmail' href='mailto:rolandmathesondev@gmail.com'><FontAwesomeIcon color='black' style={{marginRight:'15px'}} icon={'envelope'} size="lg" /></a>
+            <a aria-label='link to Rolands Github' href='https://github.com/rj13371'><FontAwesomeIcon color='black' style={{marginRight:'15px'}} icon={['fab', 'github']} size="lg" /></a>
+            <a aria-label='link to Rolands linkedin' href='https://www.linkedin.com/in/roland-matheson-b37a09224/'><FontAwesomeIcon color='black' style={{marginRight:'15px'}} icon={['fab', 'linkedin']} size="lg" /></a>
+            <a aria-label='link to Rolands medium' href='https://rolandmatheson.medium.com/'><FontAwesomeIcon color='black' style={{marginRight:'15px'}} icon={['fab', 'medium']} size="lg" /></a>
+            <a aria-label='email for rolands gmail' href='mailto:rolandmathesondev@gmail.com'><FontAwesomeIcon color='black' style={{marginRight:'15px'}} icon={'envelope'} size="lg" /></a>
         
 
       </Fragment>
